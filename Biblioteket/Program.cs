594196c781bd9b9f13fc899efd51237c30d6e5b8
RobinJohansson1992 <robin-johansson@live.com>
2025-09-26 09:28:56 +0200

    Added method DisplayBooks that shows the available books in the library. Also realised that I want to be able to get the book info in multiple methods, therefore i put the "bookInfo" directly under class Program.

--- Biblioteket/Program.cs
+++ Biblioteket/Program.cs
@@ -1,26 +1,117 @@
 ﻿namespace Biblioteket
 {
+
+    
     internal class Program
     {
+        static (int bookIndex, string books, int available)[] bookInfo =
+
+
+            {
+                (1, "Harry Potter", 2),
+                (2, "Sagan om ringen", 1),
+                (3, "Bamse i trollskogen", 4),
+                (4, "Throne of glass", 1),
+                (5, "A  song of ice and fire", 2)
+            };
+
+
         static void Main(string[] args)
         {
 
             LogIn();
 
+            Menu();
+            
+        }
+
+        static void LogIn()
+        {
+            (string userName, int code)[] users =
+            {
+                ("Chewy", 4466),
+                ("Luke", 1234),
+                ("Han", 1992),
+                ("Leia", 9999),
+                ("Yoda", 1337)
+            };
+            Console.Clear();
+            Console.WriteLine("Välkommen till bibliotekets lånesystem! \nAnge användarnamn och pinkod för att logga in:");
+
+            bool success = false;
+
+            int attempts = 3;
+
+            while (!success)
+            {
+
+                Console.Write("Användarnamn:"); string user1 = Console.ReadLine();
+                Console.Write("PIN-kod:"); int pin;
+                while (!int.TryParse(Console.ReadLine(), out pin))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Din pinkod kan endast bestå av heltal.");
+                    Console.ResetColor();
+                }
+
+                foreach (var user in users)
+                {
+                    if (user.userName == user1 && user.code == pin)
+                    {
+                        success = true;
+                        break;
+                    }
+                }
+
+                if (success)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Välkommen {user1}! \n");
+                    Console.ResetColor();
+                    Console.WriteLine("Tryck valfri knapp för att gå vidare.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Felaktigt användarnamn eller PIN-kod.");
+                    Console.ResetColor();
+                    attempts--;
+                }
+
+                if (attempts <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du klarade inte att logga in på 3 försök. \nProgrammet avslutas...");
+                    Console.ResetColor();
+                    Environment.Exit(0);
+                }
+
+            }
+
+
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
             Console.WriteLine("Vad vill du göra?");
             Console.WriteLine("1. Visa böcker \n2. Låna bok \n3. Lämna tillbaka bok \n4. Mina lån \n5. Logga ut");
 
-            int userChoise = -1;
+            int userChoise;
 
             while (!int.TryParse(Console.ReadLine(), out userChoise) || userChoise < 1 || userChoise > 5)
             {
                 Console.WriteLine("Du måste ange ett nummer från listan.");
             }
 
+
             switch (userChoise)
             {
                 case 1:
-                    Console.WriteLine("Visa böcker");
+                    DisplayBooks();
                     break;
 
                 case 2:
@@ -37,73 +128,29 @@
 
                 case 5:
                     Console.WriteLine("Logga ut");
-                    Console.WriteLine("Du loggas ut...");
-                    Console.Clear();
+                    Console.WriteLine("Du loggas ut... Tryck enter för att återgå till startsidan.");
+                    Console.ReadLine();
+                    
                     LogIn();
                     break;
             }
         }
 
-        static void LogIn()
+        static void DisplayBooks()
         {
-            (string userName, int code)[] users =
+            Console.Clear();
+            Console.WriteLine("Tillgängliga böcker:");
+
+            foreach (var book in bookInfo)
             {
-                ("Chewy", 4466),
-                ("Luke", 1234),
-                ("Han", 1992),
-                ("Leia", 9999),
-                ("Yoda", 1337)
-            };
-
-            Console.WriteLine("Välkommen till bibliotekets lånesystem! \nAnge användarnamn och lösenkod för att logga in:");
-
-            bool success = false;
-
-            int attempts = 3;
-
-            while (!success)
-            {
-
-                Console.Write("Användarnamn:"); string user1 = Console.ReadLine();
-                Console.Write("PIN-kod:"); int pin;
-                while (!int.TryParse(Console.ReadLine(), out pin))
-                {
-                    Console.WriteLine("Din pinkod kan endast bestå av heltal.");
-                }
-
-                foreach (var user in users)
-                {
-                    if (user.userName == user1 && user.code == pin)
-                    {
-                        success = true;
-                        break;
-                    }
-                }
-
-                if (success)
-                {
-                    Console.WriteLine($"Välkommen {user1}! \nTryck valfri knapp för att gå vidare.");
-                    Console.ReadLine();
-                    Console.Clear();
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Felaktigt användarnamn eller PIN-kod.");
-                    attempts--;
-                }
-
-                if (attempts <= 0)
-                {
-                    Console.WriteLine("Du klarade inte att logga in på 3 försök. \nProgrammet avslutas...");
-                    Environment.Exit(0);
-                }
-
+                Console.WriteLine($"{book.bookIndex}. {book.books}, antal: {book.available}");
             }
-
-
-
-
+            Console.WriteLine("Tryck enter för att återgå till menyn.");
+            Console.ReadLine();
+            Menu();
         }
+
+
+        
     }
 }
